345f4f2ca6e630418707223db3f529741b52caf6
ayushch01ac <65771720+ayushch01ac@users.noreply.github.com>
2023-03-12 18:11:31 +0000

    Added dynamic data and API route

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+TEAMS = [
+  {
+    'id':1,
+    'Name':'Arsenal',
+    'Points':66,
+    'GD': '+37'
+  },
+  {
+    'id':2,
+    'Name':'Manchester City',
+    'Points':61,
+    'GD': '+42'
+  },
+  {
+    'id':3,
+    'Name':'Manchester United',
+    'Points':50,
+    'GD': '+8'
+  }
+]
+
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', teams=TEAMS)
+
+@app.route("/api/teams")
+def list_teams():
+  return jsonify(TEAMS)
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
